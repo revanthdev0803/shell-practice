94730bf62c039341c85070175fb7cca28d966cb4
revanth <revanth.sn@gmail.com>
2025-05-19 11:10:57 +0530

    first

---
+++ 02-conversation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+person1=trump
+person2=musk
+
+echo "trump: hey musk, how are you?"
+echo "musk: hello trump, i am fine. how are you doing?"
+echo "trump: i am fine too. whats up"
+echo  "musk: nothing, just going to mars now, will you come"
+echo "trunp: sorry, you carry on! i will come once you come back"
+
+
+
+
---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+person1=trump
+person2=musk
+
+echo "$person1: hey $person2, how are you?"
+echo "$person2: hello $person1, i am fine. how are you doing?"
+echo "$person1: i am fine too. whats up"
+echo  "$person2: nothing, just going to mars now, will you come"
+echo "$person1: sorry, you carry on! i will come once you come back"
+
