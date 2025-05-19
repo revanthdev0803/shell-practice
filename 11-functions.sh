ee02f3adcce1c338ec85ec2cd7d62608c1aabd7a
revanth <revanth.sn@gmail.com>
2025-05-19 16:52:00 +0530

    first

--- 11-functions.sh
+++ 11-functions.sh
@@ -10,22 +10,25 @@ else
 fi
 
 
+VALIDATE(){
+
+    if [ $1 -eq 0 ]
+    then
+        echo "$2 is succesfull"
+    else
+        echo "$2 is fail"
+        exit 1
+    fi
+}
+
 dnf list installed mysql
 #if installed or not
-
 
 if [ $? -ne 0 ]
 then
     echo "mysql is not installed....going to install it"
     dnf install mysql -y
-
-    if [ $? -eq 0 ]
-    then
-        echo "my sql is succesfull"
-    else
-        echo "my sql is fail"
-        exit 1
-    fi
+    VALIDATE $?
 else
     echo "mysql is allready installed.....nothing to do"
 fi
@@ -39,13 +42,7 @@ then
     echo "python3 is not installed....going to install it"
     dnf install python3 -y
 
-    if [ $? -eq 0 ]
-    then
-        echo "python3 is succesfull"
-    else
-        echo "python3 is fail"
-        exit 1
-    fi
+    VALIDATE $?
 else
     echo "python3 is allready installed.....nothing to do"
 fi
@@ -58,14 +55,8 @@ if [ $? -ne 0 ]
 then
     echo "nginx is not installed....going to install it"
     dnf install nginx -y
-
-    if [ $? -eq 0 ]
-    then
-        echo "nginx is succesfull"
-    else
-        echo "nginx is fail"
-        exit 1
-    fi
+    VALIDATE $?
+    
 else
     echo "nginx is allready installed.....nothing to do"
 fi
