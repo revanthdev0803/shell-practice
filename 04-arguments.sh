ee1ff35dbce9551e390102a3f879025b834081f9
revanth <revanth.sn@gmail.com>
2025-05-19 11:21:01 +0530

    first

---
+++ 04-arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+person1=$1
+person2=$2
+
+echo "$person1: hey $person2, how are you?"
+echo "$person2: hello $person1, i am fine. how are you doing?"
+echo "$person1: i am fine too. whats up"
+echo  "$person2: nothing, just going to mars now, will you come"
+echo "$person1: sorry, you carry on! i will come once you come back"
