8023b78445e26796512b228a9ce02ea50c112e77
revanth <revanth.sn@gmail.com>
2025-05-19 14:00:38 +0530

    first

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -2,3 +2,10 @@
 
 echo "all variables passed to the script: $@"
 echo "number of variables:$#"
+echo "script name:$0"
+echo "current directry: $PWD"
+echo "user running this script: $USER"
+echo "home directory of user : $HOME"
+sleep 10 &
+echo "pid of last command : $!"
+
