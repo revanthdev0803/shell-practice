b2c3e47db3829c7aa30c8753e02361aa2958513d
revanth <revanth.sn@gmail.com>
2025-05-19 14:54:33 +0530

    first

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -7,6 +7,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "please run with root user"
+    exit 1 #give any number except zero for checking status
 else
     echo "you are the root user"
 fi
