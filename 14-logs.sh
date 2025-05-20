4a77a492b7365cdab55f4b116de7c4960ab1f74e
revanth <revanth.sn@gmail.com>
2025-05-20 11:49:33 +0530

    first

--- 14-logs.sh
+++ 14-logs.sh
@@ -10,12 +10,19 @@ G="\e[32m" #green
 Y="\e[33m" #yellow
 N="\e[0m" #no colour
 
+LOGS_FOLDER="/var/log/shellcripts-logs" #we create this path for storing our logs
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1) # it removes the .sh in script name
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NMAE.log"
+
+mkdir -p $LOGS_FOLDER #we are making directry and telling if it is already dont give error by using -p
+echo "script started at : $(date)" &>>LOG_FILE
+
 #we are checking that if user was not equal to zero or not
 #root user id will be zero,if not we will give error
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R Errorr....$N please run with root user"
+    echo -e "$R Errorr....$N please run with root user" &>>LOG_FILE 
     exit 1 #give any number except zero for checking status
 else
     echo "you are the root user"
@@ -28,49 +35,49 @@ VALIDATE(){
 
     if [ $1 -eq 0 ]
     then
-        echo -e " installing $2 is $G succesfull $N"
+        echo -e " installing $2 is $G succesfull $N" &>>LOG_FILE
     else
-        echo "installing $2 is fail"
+        echo "installing $2 is fail" &>>LOG_FILE
         exit 1
     fi
 }
 
-dnf list installed mysql
+dnf list installed mysql &>>LOG_FILE
 #this command will check is mysql is  installed or not
 
 if [ $? -ne 0 ]
 then
-    echo "mysql is not installed....going to install it"
+    echo "mysql is not installed....going to install it" &>>LOG_FILE
     dnf install mysql -y
     VALIDATE $? "mysql"
 else
-    echo -e "mysql is allready installed.....$Y nothing to do $N"
+    echo -e "mysql is allready installed.....$Y nothing to do $N" &>>LOG_FILE
 fi
 
-dnf list installed python3
+dnf list installed python3 &>>LOG_FILE
 #if installed or not
 
 
 if [ $? -ne 0 ]
 then
-    echo "python3 is not installed....going to install it"
+    echo "python3 is not installed....going to install it" &>>LOG_FILE
     dnf install python3 -y
 
     VALIDATE $? "python3"
 else
-    echo "python3 is allready installed.....nothing to do"
+    echo "python3 is allready installed.....nothing to do" &>>LOG_FILE
 fi
 
-dnf list installed nginx
+dnf list installed nginx &>>LOG_FILE
 #if installed or not
 
 
 if [ $? -ne 0 ]
 then
-    echo "nginx is not installed....going to install it"
+    echo "nginx is not installed....going to install it" &>>LOG_FILE
     dnf install nginx -y
     VALIDATE $? "nginx"
     
 else
-    echo "nginx is allready installed.....nothing to do"
+    echo "nginx is allready installed.....nothing to do" &>>LOG_FILE
 fi
