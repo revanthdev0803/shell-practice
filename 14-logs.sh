d6e185dda6b2de4abb675940ff14f866b34addc7
revanth <revanth.sn@gmail.com>
2025-05-20 15:38:01 +0530

    first

--- 14-logs.sh
+++ 14-logs.sh
@@ -10,22 +10,22 @@ G="\e[32m" #green
 Y="\e[33m" #yellow
 N="\e[0m" #no colour
 
-#LOGS_FOLDER="/var/log/shellcripts-logs" #we create this path for storing our logs
-#SCRIPT_NAME=$(echo $0 | cut -d "." -f1) # it removes the .sh in script name
-#LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOGS_FOLDER="/var/log/shellcripts-logs"    #we create this path for storing our logs
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)  # it removes the .sh in script name
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-#mkdir -p $LOGS_FOLDER #we are making directry and telling if it is already dont give error by using -p
-#echo "script started at: $(date)" &>>$LOG_FILE
+mkdir -p $LOGS_FOLDER #we are making directry and telling if it is already dont give error by using -p
+echo "script started at: $(date)" &>>$LOG_FILE
 
 #we are checking that if user was not equal to zero or not
 #root user id will be zero,if not we will give error
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R Errorr....$N please run with root user" &>>$LOG_FILE 
+    echo -e "$R Erorr....$N please run with root user" &>>$LOG_FILE 
     exit 1 #give any number except zero for checking status
 else
-    echo "you are the root user" &>>$LOG_FILE
+    echo "you are the root user"
 fi
 
  #we use this function if given one is installed or not ,to reduce the steps we use this
